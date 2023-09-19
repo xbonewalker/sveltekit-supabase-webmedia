b71e887449d15f179cefc17f8b7cdd7d1ac71524
X-Bone Walker <xbonewalker@proton.me>
2023-09-19 10:37:35 +0900

    Implement user authorization

---
+++ src/routes/api/articles/+server.ts
@@ -0,0 +1,11 @@
+import { json, error } from '@sveltejs/kit';
+
+export const GET = async ({ locals: { supabase, getSession } }) => {
+  const session = await getSession();
+  if (!session) {
+    throw error(401, { message: 'Unauthorized' });
+  }
+  const { data } = await supabase.from('articles').select('*');
+
+  return json({ data });
+};
