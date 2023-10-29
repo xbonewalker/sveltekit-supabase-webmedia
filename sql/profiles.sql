22ce8ed9ffcc01173e577f1544867a3d373fbd59
X-Bone Walker <xbonewalker@proton.me>
2023-10-29 16:26:08 +0900

    Update profiles.sql

--- sql/profiles.sql
+++ sql/profiles.sql
@@ -17,7 +17,29 @@ create trigger handle_updated_at before update on public.profiles
 
 alter table public.profiles enable row level security;
 
+create policy "Enable all actions for users based on id" on public.profiles
+as permissive for all
+to public
+using (auth.uid() = id)
+with check (auth.uid() = id);
+
 create policy "Enable read access for all users" on public.profiles
 as permissive for select
 to public
 using (true);
+
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.profiles (id, username)
+  values (new.id, new.raw_user_meta_data->>'username');
+  return new;
+end;
+$$;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
